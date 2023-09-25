965854fabd206ae1fc5e033a584ce3fb5acbb93d
David Actis <davidactisp@gmail.com>
2023-09-25 08:52:13 -0300

    projects and infos

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,7 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+    nameUser: "David Actis",
+    githubUser: "dfap2",
+    linkedinUser: "davidactis",
+    whatsappNumber: "+5521981297836",
+    emailUser: "davidactisp@gmail.com",
 };
